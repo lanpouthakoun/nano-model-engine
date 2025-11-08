5dee5d1c6f6865f4918abac24124f6fb89f6964f
lanpouthakoun <andrewlanpouthakoun@gmail.com>
2025-11-08 12:48:15 -0800

    Finished RMS Norm, started rotary embedding, and causallm class. Only attention left and rotary embedding

--- models/llama.py
+++ models/llama.py
@@ -5,6 +5,7 @@ Implementation of LLama with Custom Attnetion Mechanism
 import torch
 from torch import nn
 from transformers import LlamaConfig
+from layers.layernorm import LlamaRMSNorm
 
 class LLamaAttention(nn.Module):
     def __init__(
@@ -14,22 +15,32 @@ class LLamaAttention(nn.Module):
             num_kv_heads: int,
             max_position: int = 4096 * 32,
             head_dim: int | None = None,
-
-
-
-
     ):
         super().__init__()
 
 
 
-class LLamaMLP(nn.Module):
-    def __init__(self,config: LlamaConfig,):
-        pass
 
-class LlamaRMSNorm(nn.Module):
-    def __init__(self, hidden_size: int, eps: float):
-        pass
+class LLamaMLP(nn.Module):
+    def __init__(self, hidden_size: int, intermediate_size: int, bias: int):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.intermediate_size = intermediate_size
+        self.gate_proj = nn.Linear(
+            self.hidden_size, self.intermediate_size, bias=bias
+        )
+        self.up_proj = nn.Linear(
+            self.hidden_size, self.intermediate_size, bias=bias
+        )
+        self.down_proj = nn.Linear(
+            self.intermediate_size, self.hidden_size, bias=bias
+        )
+        self.act_fn = nn.SiLU()
+    
+    def forward(self, x):
+        return self.down_proj(
+            self.act_fn(self.gate_proj(x)) * self.up_proj(x)
+        )
 
 class LLamaDecoderLayer(nn.Module):
     def __init__(self, config: LlamaConfig, layer_idx: int):
@@ -37,7 +48,7 @@ class LLamaDecoderLayer(nn.Module):
         self.hidden_size = config.hidden_size
         self.self_attn = LLamaAttention(config = config, layer_idx = layer_idx)
 
-        self.mlp = LLamaMLP(config)
+        self.mlp = LLamaMLP(config.hidden_size, config.intermediate_size, config.mlp_bias)
 
         self.input_layernorm = LlamaRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
         self.post_attention_layernorm = LlamaRMSNorm(
@@ -61,7 +72,8 @@ class LLamaDecoderLayer(nn.Module):
         hidden_states = self.mlp(hidden_states)
         return hidden_states, residual
 
-class LLamaModel(nn.Module):
+class LlamaModel(nn.Module):
+    
     def __init__(self, config: LlamaConfig):
         super().__init__()
         self.padding_idx = config.pad_token_id
@@ -71,7 +83,6 @@ class LLamaModel(nn.Module):
         )
         self.layers = nn.ModuleList([LLamaDecoderLayer(config) for _ in range(config.num_hidden_layers)])
         self.norm = LlamaRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
-    
 
     def forward(
             self, input_ids: torch.Tensor, positions: torch.Tensor
@@ -86,5 +97,25 @@ class LLamaModel(nn.Module):
 
 
 class LLamaForCausalLM(nn.Module):
-    def __init__(self):
+    def __init__(self, config: LlamaConfig):
+        super().__init__()
+        self.model = LlamaModel(config)
+        self.lm_head = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
+        if config.tie_word_embeddings:
+            self.lm_head.weight.data = self.model.embed_tokens.weight.data
+
+    def forward(
+        self,
+        input_ids: torch.Tensor,
+        positions: torch.Tensor,
+    ) -> torch.Tensor:
+        return self.model(input_ids, positions)
+
+    def compute_logits(
+        self,
+        hidden_states: torch.Tensor,
+    ) -> torch.Tensor:
+        return self.lm_head(hidden_states)
+    
+    def load_model(self, model):
         pass
